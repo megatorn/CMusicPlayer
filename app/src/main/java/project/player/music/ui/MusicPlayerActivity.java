050ea5e72d793cda5ed4e3b5b036539bb4eedf55
lijie <lijie9@hikvision.com.cn>
2016-12-20 18:46:44 +0800

    [CHG]增加了ToolBar的设置和基本内容，侧滑效果有了；下一步需要完成主界面list的显示

--- app/src/main/java/project/player/music/ui/MusicPlayerActivity.java
+++ app/src/main/java/project/player/music/ui/MusicPlayerActivity.java
@@ -1,8 +1,14 @@
 package project.player.music.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import project.player.music.R;
 import project.player.music.utils.LogTool;
@@ -15,6 +21,14 @@ public class MusicPlayerActivity extends AppCompatActivity
 {
     private static final String TAG = LogTool.makeLogTag(MusicPlayerActivity.class);
 
+    private Toolbar mToolBar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private boolean mToolbarInitialized;
+    private int mItemToOpenWhenDrawerCloses = -1;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -22,5 +36,147 @@ public class MusicPlayerActivity extends AppCompatActivity
 
         LogTool.i(TAG, "Main activity created!");
         setContentView(R.layout.activity_main);
+
+        initializeToolbar();
+    }
+
+    /**菜单点击处理*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mDrawerToggle != null && mDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        if(item != null && item.getItemId() == android.R.id.home)
+        {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**DrawerLayout的监听事件*/
+    private final DrawerLayout.DrawerListener mDrawerListener = new DrawerLayout.DrawerListener()
+    {
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset)
+        {
+            if(mDrawerToggle != null)
+            {
+                mDrawerToggle.onDrawerSlide(drawerView, slideOffset);
+            }
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView)
+        {
+            if(mDrawerToggle != null)
+            {
+                mDrawerToggle.onDrawerOpened(drawerView);
+            }
+
+            if(getSupportActionBar() != null)
+            {
+                getSupportActionBar().setTitle(R.string.app_name);
+            }
+        }
+
+        @Override
+        public void onDrawerClosed(View drawerView)
+        {
+            if(mDrawerToggle != null)
+            {
+                mDrawerToggle.onDrawerClosed(drawerView);
+            }
+
+            //后续添加关闭相应的操作
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState)
+        {
+            if(mDrawerToggle != null)
+            {
+                mDrawerToggle.onDrawerStateChanged(newState);
+            }
+        }
+    };
+
+    private void initializeToolbar()
+    {
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        if(mToolBar == null)
+        {
+            throw new IllegalStateException("Layout require a toolbar");
+        }
+        mToolBar.inflateMenu(R.menu.main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
+        if(mDrawerLayout != null)
+        {
+            mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+            if(mNavigationView == null)
+            {
+                throw new IllegalStateException("Layout require a NavigationView");
+            }
+
+            //新建一个toggle控制drawer的开关
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar, R.string.open_content_drawer, R.string.close_content_drawer);
+
+            mDrawerLayout.setDrawerListener(mDrawerListener);
+
+            populateDrawerItems(mNavigationView);
+            setSupportActionBar(mToolBar);
+            updataDrawerToggle();
+        }
+        else
+        {
+            setSupportActionBar(mToolBar);
+        }
+
+        mToolbarInitialized = false;
+    }
+
+    private void populateDrawerItems(NavigationView view)
+    {
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item)
+            {
+                item.setChecked(true);
+                mItemToOpenWhenDrawerCloses = item.getItemId();
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        //打开相应的Intent
+    }
+
+    private void updataDrawerToggle()
+    {
+        if(mDrawerToggle == null)
+        {
+            return;
+        }
+
+        boolean isRoot = getFragmentManager().getBackStackEntryCount() == 0;
+        mDrawerToggle.setDrawerIndicatorEnabled(isRoot);
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().setDisplayShowHomeEnabled(!isRoot);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(!isRoot);
+            getSupportActionBar().setHomeButtonEnabled(!isRoot);
+        }
+
+        if(isRoot)
+        {
+            //将开关图片和actionBar关联
+            mDrawerToggle.syncState();
+        }
     }
 }
